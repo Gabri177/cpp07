3aa4a9fd73d480ddeee95bd7269e0ee2c242700c
GGabriel003 <108942046+GGabriel003@users.noreply.github.com>
2024-07-10 23:17:08 +0200

    finish ex00

--- ex00/Utiles.hpp
+++ ex00/Utiles.hpp
@@ -1,4 +1,29 @@
 #ifndef HEAD_UTILES_HPP
 # define HEAD_UTILES_HPP
 
+template <typename T>
+void 	swap(T & a, T & b){
+
+	T	temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T		min(T num1, T num2){
+
+	if (num2 <= num1)
+		return num2;
+	return num1;
+}
+
+template <typename T>
+T		max(T num1, T num2){
+
+	if (num2 >= num1)
+		return num2;
+	return num1;
+}
+
 #endif
